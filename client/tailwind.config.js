b25830eec4f3d30abf28446a94e7e47641798a1c
bianca081002 <b.maier15@icloud.com>
2024-12-14 17:44:13 +0200

    Create and run the server

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,11 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/*/.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Modificare: "/" include toate subdirectoarele
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [],
 }
